d0986b27f272db9da04eb3e5fb70716b765eda57
Alexandr Neshev <56019760+Sistem-Pack@users.noreply.github.com>
2026-01-25 15:21:08 +0500

    iter1 complete for check

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,64 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/Sistem-Pack/go-url-shortener/pkg/shortid"
+)
+
+var urlStorage = make(map[string]string)
+
+func checkRequestMethod(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		postProcessing(res, req)
+	case http.MethodGet:
+		getProcessing(res, req)
+	default:
+		http.Error(res, "Метод не поддерживается", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func postProcessing(res http.ResponseWriter, req *http.Request) {
+	body, err := io.ReadAll(req.Body)
+	if err != nil || len(body) == 0 {
+		http.Error(res, "Некорректное тело запроса", http.StatusBadRequest)
+		return
+	}
+
+	id, err := shortid.Generate()
+	urlStorage[id] = string(body)
+
+	shortURL := fmt.Sprintf("http://localhost:8080/%s", id)
+
+	res.WriteHeader(http.StatusCreated)
+	res.Header().Set("Content-Type", "text/plain")
+	res.Header().Set("Content-Length", fmt.Sprintf("%d", len(shortURL)))
+	res.Write([]byte(shortURL))
+}
+
+func getProcessing(res http.ResponseWriter, req *http.Request) {
+	id := req.URL.Path[1:]
+
+	originalURL, located := urlStorage[id]
+	if !located || id == "" {
+		http.Error(res, "Некорректный ID", http.StatusBadRequest)
+		return
+	}
+
+	res.Header().Set("Location", originalURL)
+	res.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, checkRequestMethod)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
